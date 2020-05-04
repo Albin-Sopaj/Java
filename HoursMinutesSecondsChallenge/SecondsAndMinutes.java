6bfaa4fe7780420f55d91f10aecf07ab37bb36a0
Albin-Sopaj <62503305+Albin-Sopaj@users.noreply.github.com>
2020-05-04 23:07:28 +0200

    Create SecondsAndMinutes.java
    The purpose of this Java program is to take some input in the form of minutes or seconds, and then convert it to the corresponding hours + minutes + seconds. The second method of the program takes only seconds, and then it converts it to hours and minutes and seconds. 
    CHEERS. HAPPY CODING WITH JAVA.

---
+++ HoursMinutesSecondsChallenge/SecondsAndMinutes.java
@@ -0,0 +1,67 @@
+//Create a method called getDurationString with two parameters, first parameter 
+//minutes and 2nd parameter seconds.
+//
+//You should validate that the first parameter minutes is >= 0.
+//
+//You should validate that the 2nd parameter seconds is >= 0 and <= 59.
+//
+//The method should return Invalid value in the method if either of the above are 
+//not true.
+//
+//If the parameters are valid then calculate how many hours, minutes and seconds equal 
+//the minutes and seconds passed to this method and return that value as string in 
+//format XXh YYm ZZs where XX represents a number of hours, YY the minutes and ZZ 
+//the seconds.
+//
+//Create a 2nd method of the same name but with only one parameter seconds.
+//
+//Validate that it is >= 0, and return Invalid value if it is not true.
+//
+//If it is valid, then calculate how many minutes are in the seconds value and then 
+//call the other overloaded method passing the correct minutes and seconds calculated 
+//so that it can calculate correctly.
+//
+//Call both methods to print values to the console.
+//
+//	1 minute = 60 seconds and 1 hour = 60 minutes or 3600 seconds.
+//
+//For the input 61 minutes output should be 01h 01m 00s, but it is ok if it is 1h 1m 0s 
+
+package secondsAndMinutesChallenge;
+
+public class SecondsAndMinutes {
+
+	public static String getDurationString(int minutes, int seconds) {
+		if (!(minutes >= 0 && seconds >= 0 && seconds <= 59)) {
+			return "Invalid Value";
+		} else {
+			int hours = (minutes / 60) + (seconds / 3600);
+			int mins = minutes % 60 + (seconds % 3600) / 60;
+			int secs = seconds % 60;
+			return hours + "h " + mins + "m " + secs + "s"; 
+		}
+	}
+//		public static String getDurationString(int seconds) {
+//			if (!(seconds >= 0)) {
+//				return "Invalid Value";
+//			} else {
+//				int minutes = seconds / 60;
+//				int secs = seconds % 60;
+//				return minutes + "m " + secs + "s";
+//			}
+//		}
+	public static String getDurationString(int seconds) {
+		if (!(seconds >= 0)) {
+			return "Invalid Value";
+		} else {
+			int minutes = seconds / 60;
+			int secs = seconds % 60;
+			String result = getDurationString(minutes, secs);
+			return result;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(getDurationString(30, 19));
+		System.out.println(getDurationString(19));
+	}
+}
